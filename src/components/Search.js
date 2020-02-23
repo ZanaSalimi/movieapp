7fa6eb9ddd2b25058e3635df3713c9f4162242b0
Zana Salmi <zanasalimi21@gmail.com>
2020-02-23 16:18:49 +0330

    Updated Search

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react'
-import SearchList from './SearchList'
 
 export class Search extends Component {
     state={
         search: '',
-        movie: []
+        options: []
     }
     onChange = (e) => {
         this.setState({ search: e.target.value })
@@ -12,7 +11,7 @@ export class Search extends Component {
             fetch(`https://api.themoviedb.org/3/search/movie?api_key=8dbc2af9039743bfdabfd4b6e58cedcc&language=en-US&query=${this.state.search}&page=1&include_adult=false`)
             .then(res => res.json())
             .then(data => {
-                this.setState({ movie: data})
+                this.setState({ options: data.results.map(movie => movie.title) })
             })
         }
     }
@@ -20,15 +19,28 @@ export class Search extends Component {
         e.preventDefault();
         this.props.search(this.state.search)
     }
+    movieSelect = (e) => {
+        this.setState({ search: e.target.value })
+    }
+    movieNames = () => {
+        if(this.state.search !== ''){
+            return this.state.options.map(name=> (
+                <option className="d-block suggestion" onClick={this.movieSelect}>{name}</option>
+            ))
+        }
+        else{
+            return''
+        }
+    }
     render() {
         return (
-            <div className="ml-auto">
-                <form className="form-inline my-2 my-lg-0" onSubmit={this.onSubmit} >
-                    <input className="form-control mr-sm-2 searchbar" type="search" name="search" placeholder="Search" aria-label="Search" value={this.state.text} onChange={this.onChange} />
-                    <ul className="suggestion-container">
-                    <SearchList movie={this.state.movie} />
-                    </ul>
+            <div className="ml-auto d-inline-block">
+                <form className="form-inline my-2 my-lg-0" autoComplete="off" onSubmit={this.onSubmit} >
+                    <input className="form-control mr-sm-2 searchbar" type="search" name="search" placeholder="Search" aria-label="Search" value={this.state.search} onChange={this.onChange} />
                 </form>
+                <div className="suggestions">
+                        {this.movieNames()} 
+                    </div>
             </div>
         )
     }
